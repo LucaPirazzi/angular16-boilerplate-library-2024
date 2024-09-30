b2568c8b6d45b5274af12c805036224a41c51d75
PC48423\lucapirazzi <luca.pirazzi@sistinf.it>
2024-09-30 17:10:26 +0200

    Update

--- projects/ui-kit-abl/src/lib/hello-ui-kit/hello-ui-kit.component.ts
+++ projects/ui-kit-abl/src/lib/hello-ui-kit/hello-ui-kit.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello-ui-kit',
   templateUrl: './hello-ui-kit.component.html',
   styleUrls: ['./hello-ui-kit.component.css']
 })
 export class HelloUiKitComponent {
-  message: string = 'Benvenuto nel tuo UI Kit!';
+  @Input() img: string = '';
+  title: string = 'Benvenuto nel tuo UI kit';
+  message: string = 'Inizia a progettare i tuoi componenti!';
 }
